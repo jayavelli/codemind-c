d68b93c4fd51501d8d217952ec725ff245fd57bd
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-02-20 11:41:45 +0530

    #include<stdio.h> int main() {     int l,r,k;     scanf("%d%d%d",&l,&r,&k);     int i,count=0;     for(i=l;i<=r;i++)     {         if(i%k==0)         {             count++;         }     }printf("%d",count); }

---
+++ Count_Divisors.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int l,r,k;
+    scanf("%d%d%d",&l,&r,&k);
+    int i,count=0;
+    for(i=l;i<=r;i++)
+    {
+        if(i%k==0)
+        {
+            count++;
+        }
+    }printf("%d",count);
+}
