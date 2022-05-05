3d51913ae1a32a8359851e2fddccd906f2f67e60
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-05-05 14:26:30 +0530

    #include<stdio.h> int main() {     int n;     int a,b,sum=0,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d%d",&a,&b);         sum=a+b;         printf("%d ",sum);     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -2,16 +2,13 @@
 int main()
 {
     int n;
+    int a,b,sum=0,i;
     scanf("%d",&n);
-    int a[n],b[n],c[n],i;
-    for(i=0;i<n;i++)
+    for(i=1;i<=n;i++)
     {
-        scanf("%d",&a[i]);
-        scanf("%d",&b[i]);
-    }
-    for(i=0;i<n;i++)
-    {
+        scanf("%d%d",&a,&b);
+        sum=a+b;
         printf("%d
-",a[i]+b[i]);
+",sum);
     }
 }
