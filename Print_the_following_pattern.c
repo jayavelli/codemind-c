120f03b08be1872957de9fef4b522f90e9a0ee89
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-10-07 18:31:41 +0530

    #include<stdio.h>
    int main()
    {
         int n;
         scanf("%d",&n);
         int i,j,c=1;
         for(i=n;i>=1;i--)
             {
                 for(j=n;j>=1;j--)
                 {
                     if(i==j||i==n+1-j)
                     {
                         printf("%d ",i);
                     }
                     else
                     {
                         printf(" ");
                     }
                     
                         
                     
                    
                 }
                
                 
                
         printf("
    ");
             }
    }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -6,14 +6,22 @@ int main()
      int n;
      scanf("%d",&n);
      int i,j,c=1;
-     for(i=1;i<=n;i++)
+     for(i=n;i>=1;i--)
          {
-             for(j=i;j<=n;j++)
+             for(j=n;j>=1;j--)
              {
+                 if(i==j||i==n+1-j)
+                 {
+                     printf("%d ",i);
+                 }
+                 else
+                 {
+                     printf(" ");
+                 }
                  
                      
                  
-                 printf("%d ",j);
+                
              }
             
              
